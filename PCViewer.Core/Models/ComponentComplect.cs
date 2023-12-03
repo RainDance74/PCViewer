a2fcb706a593c3d10aed576ac24595162a643274
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2023-12-03 22:23:58 +0500

    fix(component complect): file rename and custom cost

---
+++ PCViewer.Core/Models/ComponentComplect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PCViewer.Core.Models
+{
+    public abstract class ComponentComplect
+    {
+        public abstract int ComplectCost { get; set; }
+    }
+
+    public class ComponentComplect<T> : ComponentComplect
+        where T : BaseComponent
+    {
+        private int _complectCost;
+        public override int ComplectCost
+        {
+            get => _complectCost == 0 ? Values.Sum(v => v.Cost) : _complectCost;
+            set => _complectCost = value;
+        }
+        public IEnumerable<T> Values { get; set; }
+
+        public ComponentComplect(params T[] components)
+        {
+            Values = components;
+        }
+
+        public ComponentComplect(int complectCost, params T[] components)
+        {
+            Values = components;
+            _complectCost = complectCost;
+        }
+    }
+}
